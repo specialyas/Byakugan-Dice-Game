b72e42a4920b8bac5cc4f9af76247a32c3e0ccb6
The Techie One <54225481+specialyas@users.noreply.github.com>
2022-05-04 03:57:43 +0100

    Add a button for rolling dice

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
-let randomNumber1 = Math.floor(Math.random() * 6) +1;
-console.log(randomNumber1)
-document.querySelector('.img1').src = `images/dice${randomNumber1}.png`
+document.querySelector('button').addEventListener('click', rollDice)
 
-let randomNumber2 = Math.floor(Math.random() * 6)+1;
-console.log(randomNumber2)
 
-document.querySelector('.img2').src = `images/dice${randomNumber2}.png`
+function rollDice(){
+    let randomNumber1 = Math.floor(Math.random() * 6) +1;
+    console.log(randomNumber1)
+    document.querySelector('.img1').src = `images/dice${randomNumber1}.png`
 
-if (randomNumber1 > randomNumber2){
-    document.querySelector('h1').innerText = "Player 1 Won"
-}else if (randomNumber1 === randomNumber2){
-    document.querySelector('h1').innerText = "Draw"
-}else {
-    document.querySelector('h1').innerText = "Player 2 Won"
+    let randomNumber2 = Math.floor(Math.random() * 6)+1;
+    console.log(randomNumber2)
+
+    document.querySelector('.img2').src = `images/dice${randomNumber2}.png`
+
+    if (randomNumber1 > randomNumber2){
+        document.querySelector('h1').innerText = "Player 1 Won"
+    }else if (randomNumber1 === randomNumber2){
+        document.querySelector('h1').innerText = "Draw"
+    }else {
+        document.querySelector('h1').innerText = "Player 2 Won"
+    }
 }
